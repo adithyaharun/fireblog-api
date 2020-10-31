963a04a8fdd8b169f1c648fbb0e4cd3a5c71eacb
Adithya Harun Tular <adithyaharun@outlook.com>
2020-11-01 02:05:19 +0800

    fix(config): load config from env

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -1,68 +1,99 @@
+require("dotenv").config();
 
-export default {
-  mode: 'spa',
+const config = {
+  mode: "spa",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    titleTemplate: (title) => title ? `${title} | Adithya` : 'Adithya',
+    titleTemplate: title => (title ? `${title} | ${process.env.APP_NAME}` : process.env.APP_NAME),
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     bodyAttrs: {
-      id: 'universe'
+      id: "universe"
     }
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/styles/universe.scss'
-  ],
+   ** Global CSS
+   */
+  css: ["~/assets/styles/universe.scss"],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    '~/plugins/axios'
+    "~/plugins/axios"
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
+    "@nuxtjs/firebase",
+    "@nuxtjs/dotenv",
+    "@nuxtjs/google-analytics"
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://asia-east2-fireball-c9a02.cloudfunctions.net/v1' : 'http://localhost:5000/fireball-c9a02/asia-east2/v1'
-    // baseURL: 'https://asia-east2-fireball-c9a02.cloudfunctions.net/v1'
+   ** Firebase
+   */
+  firebase: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    },
+    onFirebaseHosting: true,
+    services: {
+      performance: true,
+      analytics: true
+    }
   },
   /*
-  ** Build configuration
-  */
+   ** Google Analytics module configuration.
+   ** See https://google-analytics.nuxtjs.org/options
+   */
+  googleAnalytics: {
+    dev: process.env.GOOGLE_ANALYTICS === false,
+    id: process.env.GOOGLE_ANALYTICS_ID
+  },
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {
+    baseURL: process.env.FIREBASE_FUNCTIONS_URL
+  },
+  /*
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
+
+export default config;
